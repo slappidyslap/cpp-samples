04deb7a51ee83e619276cd97ceed5599078e49cb
Eldiyar <musabaeveldiar@gmail.com>
2025-09-25 21:47:14 +0600

    подключение стат либы

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,32 @@
 #include <iostream>
-// #include "sfml_test.cpp"
-// #include "sdl_test.cpp"
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
 
 using std::string;
 
 int main() {
-    string a = "";
-    return 0;
+    auto window = sf::RenderWindow(sf::VideoMode({1920, 1080}), "CMake SFML Project");
+    window.setFramerateLimit(144);
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+            {
+                window.close();
+            }
+        }
+        window.clear(Color(250, 220, 100, 0));
+
+        CircleShape circle(50.f);
+		circle.setFillColor(Color(230, 0, 230)); // закрашиваем наш круг 
+		// circle.setOutlineThickness(S15.f); // устанавливаем толщину контура круга
+		circle.setOutlineColor(Color(80,220,50)); // устанавливаем цвет контура
+		circle.move({15, 15}); // перемещаем наш круг для корректного отображения в окне
+		window.draw(circle);
+        
+        window.display();
+    }
 }
